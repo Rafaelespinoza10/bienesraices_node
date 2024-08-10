3d265aaf3e736c42f5802ada836b1881f6d35d34
Rafael_Espinoza~ <rafael.moreno.espinoza10@gmail.com>
2024-08-09 20:59:13 -0600

    fix

--- config/db.js
+++ config/db.js
@@ -4,20 +4,28 @@ import dotenv from 'dotenv';
 dotenv.config({path: '.env'});
 // Conexion a la base de datos
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS,{
-    host: process.env.DB_HOST,
+    host: process.env.BD_HOST,
     port: 3307, 
     dialect: 'mysql',
+    logging: console.log, // Muestra logs SQL en la consola
     define:{
         timestamps: true
     },
     pool:{  /// configura como va a ser el comportamiento para nuevas conexiones
-        max:5,
+        max:10,
         min:0,
         acquire:30000,   // 30 s  tiempo antes de marcar un error
         idle:10000,
     },
-    operatorsAliases: false, 
+    //operatorsAliases: false, 
 });
+
+console.log(process.env.BD_NOMBRE)
+console.log(process.env.BD_USER)
+console.log(process.env.BD_PASS)
+console.log(process.env.BD_HOST)
+
+
 
 export default db;
 
